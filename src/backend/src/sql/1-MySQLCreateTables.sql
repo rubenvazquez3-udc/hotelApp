bca8f81b3d58094b61e2e643b41be84fccf374b0
Ruben Vazquez Fariña <ruben.vazquez3@udc.es>
2020-07-05 17:56:11 +0200

    Arreglado error en SQL

--- src/backend/src/sql/1-MySQLCreateTables.sql
+++ src/backend/src/sql/1-MySQLCreateTables.sql
@@ -1,21 +1,61 @@
 -- Indexes for primary keys have been explicitly created.
 
-DROP TABLE User;
-DROP TABLE Address;
-DROP TABLE Country;
-DROP TABLE Account;
-DROP TABLE Reservation;
 DROP TABLE AccountItem;
-DROP TABLE Product;
+DROP TABLE Account;
+DROP TABLE RoomService;
+DROP TABLE RoomReservation;
+DROP TABLE Room;
+DROP TABLE RoomTypeReservation;
+DROP TABLE RoomType;
+DROP TABLE Address;
+DROP TABLE GuestReservation;
+DROP TABLE Reservation;
 DROP TABLE Service;
+DROP TABLE User;
+DROP TABLE Product;
 DROP TABLE Hotel;
 DROP TABLE Guest;
-DROP TABLE GuestReservation;
-DROP TABLE Room;
-DROP TABLE RoomReservation;
-DROP TABLE RoomService;
-DROP TABLE RoomType;
-DROP TABLE RoomTypeReservation;
+DROP TABLE Country;
+
+CREATE TABLE Country(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(75) NOT NULL,
+    CONSTRAINT CountryPK PRIMARY KEY (id),
+    CONSTRAINT CountryNameUniqueKey UNIQUE (name)
+) ENGINE = InnoDB;
+
+CREATE TABLE Guest (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(60) NOT NULL,
+    surname VARCHAR(65) NOT NULL,
+    identifier VARCHAR(4) NOT NULL,
+    address VARCHAR(75) NOT NULL,
+    dni VARCHAR(10) NOT NULL,
+    CONSTRAINT GuestPK PRIMARY KEY (id),
+    CONSTRAINT GuestDniUniqueKey UNIQUE (dni)
+) ENGINE = InnoDB;
+
+CREATE INDEX GuestIndexByName ON Guest (name);
+
+CREATE TABLE Hotel (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    address VARCHAR(75) NOT NULL,
+    manager VARCHAR(30) NOT NULL,
+    CONSTRAINT HotelPK PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE INDEX HotelIndexByName ON Hotel (name);
+
+CREATE TABLE Product (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    description VARCHAR(75) NOT NULL,
+    price DECIMAL(11,2) NOT NULL,
+    CONSTRAINT ProductPK PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE INDEX ProductIndexByName ON Product (name);
 
 CREATE TABLE User (
     id BIGINT NOT NULL AUTO_INCREMENT,
@@ -31,75 +71,11 @@ CREATE TABLE User (
 
 CREATE INDEX UserIndexByUserName ON User (userName);
 
-CREATE TABLE Address (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    state VARCHAR(30),
-    road VARCHAR(60),
-    city VARCHAR(45),
-    countryId BIGINT,
-    userId BIGINT NOT NULL,
-    CONSTRAINT AddressPK PRIMARY KEY(id),
-    CONSTRAINT UserAddressFK FOREIGN KEY(userId)
-        REFERENCES User(id),
-    CONSTRAINT CountryAddressFK FOREIGN KEY(countryId)
-        REFERENCES Country(id)
-) ENGINE = InnoDB;
-
-CREATE TABLE Country(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(75) NOT NULL,
-    CONSTRAINT CountryPK PRIMARY KEY (id),
-    CONSTRAINT CountryNameUniqueKey UNIQUE (name)
-) ENGINE = InnoDB;
-
-CREATE TABLE Account (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    reservationId BIGINT NOT NULL,
-    in DATETIME NOT NULL,
-    out DATETIME NOT NULL,
-    CONSTRAINT AccountPK PRIMARY KEY (id),
-    CONSTRAINT AccountReservationFK FOREIGN KEY (reservationId)
-        REFERENCES Reservation(id)
-) ENGINE = InnoDB;
-
-CREATE TABLE Reservation(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    userId BIGINT,
-    inbound DATETIME NOT NULL,
-    outbound DATETIME NOT NULL,
-    CONSTRAINT ReservationPK PRIMARY KEY (id),
-    CONSTRAINT ReservationUserFK FOREIGN KEY(userId)
-        REFERENCES User(id)
-) ENGINE = InnoDB;
-
-CREATE TABLE AccountItem (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    accountId BIGINT NOT NULL,
-    quantity SMALLINT NOT NULL,
-    itemPrice DECIMAL (11,2),
-    productId BIGINT NOT NULL,
-    CONSTRAINT AccountITemPK PRIMARY KEY (id),
-    CONSTRAINT AccountItemAccountFK FOREIGN KEY (accountId)
-        REFERENCES Account(id),
-    CONSTRAINT AccountItemProductFK FOREIGN KEY (productId)
-        REFERENCES Product(id)
-) ENGINE = InnoDB;
-
-CREATE TABLE Product (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    description VARCHAR(75) NOT NULL,
-    price DECIMAL (11,2) NOT NULL,
-    CONSTRAINT ProductPK PRIMARY KEY (id)
-) ENGINE = InnoDB;
-
-CREATE INDEX ProductIndexByName ON Product (name);
-
 CREATE TABLE Service (
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     description VARCHAR(75) NOT NULL,
-    price DECIMAL (11,2) NOT NULL,
+    price DECIMAL(11,2) NOT NULL,
     hotelId BIGINT NOT NULL,
     CONSTRAINT ServicePK PRIMARY KEY (id),
     CONSTRAINT ServiceHotelFK FOREIGN KEY (hotelId)
@@ -108,41 +84,15 @@ CREATE TABLE Service (
 
 CREATE INDEX ServiceIndexByName ON Service (name);
 
-CREATE TABLE Hotel (
+CREATE TABLE Reservation(
     id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    address VARCHAR(75) NOT NULL,
-    manager VARCHAR (30) NOT NULL,
-    CONSTRAINT HotelPK PRIMARY KEY (id)
+    userId BIGINT,
+    inbound DATETIME NOT NULL,
+    outbound DATETIME NOT NULL,
+    CONSTRAINT ReservationPK PRIMARY KEY (id),
+    CONSTRAINT ReservationUserFK FOREIGN KEY (userId)
+        REFERENCES User (id)
 ) ENGINE = InnoDB;
-
-CREATE INDEX HotelIndexByName ON Hotel (name);
-
-CREATE TABLE RoomService (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    date  DATETIME NOT NULL,
-    status VARCHAR (20) NOT NULL,
-    userId BIGINT NOT NULL,
-    roomId BIGINT NOT NULL,
-    CONSTRAINT RoomServicePK PRIMARY KEY (id),
-    CONSTRAINT RoomServiceUserFK FOREIGN KEY (userId)
-        REFERENCES User(id),
-    CONSTRAINT RoomServiceRoomIdFK FOREIGN KEY (roomId)
-        REFERENCES Room(id)
-) ENGINE = InnoDB;
-
-CREATE TABLE Guest (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(60) NOT NULL,
-    surname VARCHAR(65) NOT NULL,
-    identifier VARCHAR(4) NOT NULL,
-    address VARCHAR (75) NOT NULL,
-    dni VARCHAR(10) NOT NULL,
-    CONSTRAINT GuestPK PRIMARY KEY (id),
-    CONSTRAINT GuestDniUniqueKey UNIQUE (dni)
-) ENGINE = InnoDB;
-
-CREATE INDEX GuestIndexByName ON Guest (name);
 
 CREATE TABLE GuestReservation(
     id BIGINT NOT NULL AUTO_INCREMENT,
@@ -150,23 +100,23 @@ CREATE TABLE GuestReservation(
     guestId BIGINT NOT NULL,
     CONSTRAINT GuestReservationPK PRIMARY KEY (id),
     CONSTRAINT GuestReservationFK FOREIGN KEY (reservationId)
-        REFERENCES Reservation(id),
+        REFERENCES Reservation (id),
     CONSTRAINT GuestReservationGuestFK FOREIGN KEY (guestId)
-        REFERENCES Guest(id)
+        REFERENCES Guest (id)
 ) ENGINE = InnoDB;
 
-CREATE TABLE Room(
+ CREATE TABLE Address (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    number BIGINT NOT NULL,
-    status VARCHAR (20) NOT NULL,
-    typeId BIGINT NOT NULL,
-    hotelId BIGINT NOT NULL,
-    CONSTRAINT RoomPK PRIMARY KEY (id),
-    CONSTRAINT RoomHotelFK FOREIGN KEY (hotelId)
-        REFERENCES Hotel(id),
-    CONSTRAINT RoomTypeFK FOREIGN KEY (typeId)
-        REFERENCES RoomType (id),
-    CONSTRAINT RoomNumberUniqueKey UNIQUE (number)
+    state VARCHAR(30),
+    road VARCHAR(60),
+    city VARCHAR(45),
+    countryId BIGINT,
+    userId BIGINT NOT NULL,
+    CONSTRAINT AddressPK PRIMARY KEY(id),
+    CONSTRAINT UserAddressFK FOREIGN KEY(userId)
+        REFERENCES User (id),
+    CONSTRAINT CountryAddressFK FOREIGN KEY(countryId)
+        REFERENCES Country (id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE RoomType (
@@ -174,6 +124,32 @@ CREATE TABLE RoomType (
     name VARCHAR(10) NOT NULL,
     CONSTRAINT RoomTypePK PRIMARY KEY (id),
     CONSTRAINT RoomTypeNameUnique UNIQUE (name)
+) ENGINE = InnoDB;
+
+CREATE TABLE RoomTypeReservation (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    typeId BIGINT NOT NULL,
+    reservationId BIGINT NOT NULL,
+    rooms SMALLINT NOT NULL,
+    CONSTRAINT RoomTypeReservationPK PRIMARY KEY (id),
+    CONSTRAINT RoomTypeReservationTypeFK FOREIGN KEY (typeId)
+        REFERENCES RoomType (id),
+    CONSTRAINT RoomTypeReservationReservationFK FOREIGN KEY (reservationId)
+        REFERENCES Reservation (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE Room(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    number BIGINT NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    typeId BIGINT NOT NULL,
+    hotelId BIGINT NOT NULL,
+    CONSTRAINT RoomPK PRIMARY KEY (id),
+    CONSTRAINT RoomHotelFK FOREIGN KEY (hotelId)
+        REFERENCES Hotel (id),
+    CONSTRAINT RoomTypeFK FOREIGN KEY (typeId)
+        REFERENCES RoomType (id),
+    CONSTRAINT RoomNumberUniqueKey UNIQUE (number)
 ) ENGINE = InnoDB;
 
 CREATE TABLE RoomReservation (
@@ -184,19 +160,44 @@ CREATE TABLE RoomReservation (
     endDate DATETIME NOT NULL,
     CONSTRAINT RoomReservationPK PRIMARY KEY (id),
     CONSTRAINT RoomReservationRoomFK FOREIGN KEY (roomId)
-        REFERENCES Room(id),
+        REFERENCES Room (id),
     CONSTRAINT RoomReservationReservationFK FOREIGN KEY (reservationId)
-        REFERENCES Reservation(id)
+        REFERENCES Reservation (id)
 ) ENGINE = InnoDB;
 
-CREATE TABLE RoomTypeReservation (
+CREATE TABLE RoomService (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    typeId BIGINT NOT NULL,
+    date  DATETIME NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    userId BIGINT NOT NULL,
+    roomId BIGINT NOT NULL,
+    CONSTRAINT RoomServicePK PRIMARY KEY (id),
+    CONSTRAINT RoomServiceUserFK FOREIGN KEY (userId)
+        REFERENCES User (id),
+    CONSTRAINT RoomServiceRoomIdFK FOREIGN KEY (roomId)
+        REFERENCES Room (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE Account (
+    id BIGINT NOT NULL AUTO_INCREMENT,
     reservationId BIGINT NOT NULL,
-    rooms SMALLINT NOT NULL,
-    CONSTRAINT RoomTypeReservationPK PRIMARY KEY (id),
-    CONSTRAINT RoomTypeReservationTypeFK FOREIGN KEY (typeId)
-        REFERENCES RoomType(id),
-    CONSTRAINT RoomTypeReservationReservationFK FOREIGN KEY (reservationId)
-        REFERENCES Reservation(id)
+    inbound DATETIME NOT NULL,
+    outbound DATETIME NOT NULL,
+    CONSTRAINT AccountPK PRIMARY KEY (id),
+    CONSTRAINT AccountReservationFK FOREIGN KEY (reservationId)
+        REFERENCES Reservation (id)
+) ENGINE = InnoDB;
+
+
+CREATE TABLE AccountItem (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    accountId BIGINT NOT NULL,
+    quantity SMALLINT NOT NULL,
+    itemPrice DECIMAL(11,2),
+    productId BIGINT NOT NULL,
+    CONSTRAINT AccountITemPK PRIMARY KEY (id),
+    CONSTRAINT AccountItemAccountFK FOREIGN KEY (accountId)
+        REFERENCES Account (id),
+    CONSTRAINT AccountItemProductFK FOREIGN KEY (productId)
+        REFERENCES Product (id)
 ) ENGINE = InnoDB;
