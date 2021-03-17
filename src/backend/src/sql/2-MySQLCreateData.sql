d986765dcb46ff152b3ba7402f30490ff0313e68
Manuel Álvarez Díaz <manuel.alvarez@udc.es>
2021-03-17 20:55:18 +0100

    [Misc] Added users

--- src/backend/src/sql/2-MySQLCreateData.sql
+++ src/backend/src/sql/2-MySQLCreateData.sql
@@ -3,3 +3,8 @@
 -- in the "pa-project" database.
 -------------------------------------------------------------------------------
 
+// password = secret
+INSERT INTO User (userName, password, firstName, lastName, email, role)
+VALUES ('admin', '$2a$10$G1YMhG.SIKtbef.Kr172p.LcSHFTze1x.L6wY.xDhDfiRg6yr10U.', 'admin', '(admin)', 'admin@udc.es', 1),
+       ('hotel', '$2a$10$G1YMhG.SIKtbef.Kr172p.LcSHFTze1x.L6wY.xDhDfiRg6yr10U.', 'hotel', '(hotel)', 'hotel@udc.es', 2),
+       ('manager', '$2a$10$G1YMhG.SIKtbef.Kr172p.LcSHFTze1x.L6wY.xDhDfiRg6yr10U.', 'manager', '(manager)', 'manager@udc.es', 3);
